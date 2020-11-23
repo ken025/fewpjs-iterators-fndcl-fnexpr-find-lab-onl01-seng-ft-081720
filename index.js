d0e32c3529e426fb79467711a57602e6f2bffc07
ken025 <kennethmanuel250@gmail.com>
2020-11-22 23:53:10 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(arr){
+  let result = arr.find(arr => arr.result === "W" )
+  // return !!result ? result.year : undefined
+  if (result){
+    return result.year
+  }
+}
